e7b32240734d4b34b8b395e145c2bf20625241c6
AcademiccMacharia <machariab19@gmail.com>
2023-07-29 15:32:25 +0300

    fixing pycodestyle errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,8 +5,8 @@ import sys
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
-    
-    user = "{}users/{}".format(url, sys.argv[1]);
+
+    user = "{}users/{}".format(url, sys.argv[1])
     result = requests.get(user)
     res = result.json()
     print("Employee {} is done with tasks".format(res.get('name')), end="")
