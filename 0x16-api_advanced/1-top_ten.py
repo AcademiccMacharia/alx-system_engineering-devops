e5e6b33051e076c15700a46a5a31a87a7da00209
AcademiccMacharia <machariab19@gmail.com>
2023-08-08 16:39:36 +0300

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Function that prints the titles of the first 10 hot posts for a subreddit"""
+
+import requests
+
+def top_ten(subreddit):
+    """Top ten hot posts"""
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'limit': 10
+    }
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    res = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if res.status_code != 200;
+    print(None)
+    return
+    
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
